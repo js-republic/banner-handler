74761c82484e206860ac4e94deaf4bd70b0afea8
Mathieu Breton <mathieu.breton@js-republic.com>
2017-12-04 20:13:38 +0100

    add google group checking

--- server/env.js
+++ server/env.js
@@ -4,6 +4,6 @@ module.exports = envalid.cleanEnv(process.env, {
   GOOGLE_CLIENT_ID : envalid.str(),
   GOOGLE_CLIENT_SECRET : envalid.str(),
   GOOGLE_CALLBACK_URL: envalid.url(),
-  GOOGLE_ALLOWED_USERS: envalid.json(),
+  GOOGLE_ALLOWED_GROUP: envalid.str(),
   PORT: envalid.port()
 });
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,35 +2,51 @@ const passport = require("passport");
 const router = require("express").Router();
 const GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
 const session = require("express-session");
-
+const axios = require('axios');
 const env = require('../env');
 
-const {userIds} = env.GOOGLE_ALLOWED_USERS;
+const GOOGLE_SCOPE = [
+  "https://www.googleapis.com/auth/plus.login",
+  "https://www.googleapis.com/auth/admin.directory.group.readonly"
+];
 
 passport.use(new GoogleStrategy({
     clientID: env.GOOGLE_CLIENT_ID,
     clientSecret: env.GOOGLE_CLIENT_SECRET,
     callbackURL: env.GOOGLE_CALLBACK_URL,
   }, (accessToken, refreshToken, profile, done) => {
-    return done(null, {
-      id: profile.id,
-      name: profile.displayName,
-      avatar: profile.photos.shift().value
-    });
+    isUserBelongToAllowedGroup()
+      .then(() => {
+        return done(null, {
+          id: profile.id,
+          name: profile.displayName,
+          avatar: profile.photos.shift().value
+        });
+      })
+      .catch(e => done(e));
   })
 );
 
 router.get("/user", (req, res) => res.json(req.user));
-router.get("/google", passport.authenticate("google", {scope: ["https://www.googleapis.com/auth/plus.login"]}));
+router.get("/google", passport.authenticate("google", {scope: GOOGLE_SCOPE}));
 router.get("/google/callback", passport.authenticate("google", {successRedirect: '/main', failureRedirect: "/login"}));
+passport.serializeUser((user, done) => done(null, user));
+passport.deserializeUser((user, done) => done(null, user));
 
-passport.serializeUser(function (user, done) {
-  done(null, user);
-});
-
-passport.deserializeUser(function (user, done) {
-  done(null, user);
-});
+function isUserBelongToAllowedGroup(accessToken, userID) {
+  return axios.get(`https://www.googleapis.com/admin/directory/v1/groups?userKey=${userID}`, {
+    "headers": {
+      "Authorization": `Bearer ${accessToken}`
+    }
+  })
+    .then(({data: {groups}}) => {
+      const foundGroup = groups.find(group => group.email === env.GOOGLE_ALLOWED_GROUP);
+      if (!foundGroup) {
+        throw new Error(`The user doesn't belong to ${env.GOOGLE_ALLOWED_GROUP}`);
+      }
+      return foundGroup;
+    })
+}
 
 module.exports = {
   handleAuthRoutes(app) {
