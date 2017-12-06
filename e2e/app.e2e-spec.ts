fbb4b90bdb32165b3d9153557ce8e5508ed91e7f
guillaumebarranco <guillaume.barranco8@hotmail.fr>
2017-12-06 14:06:07 +0100

    add e2e tests

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,7 +1,8 @@
 import { AppPage } from './app.po';
 import { browser, by, element } from 'protractor';
+import * as path from 'path';
 
-describe('bannerhandler App', () => {
+describe('Banner Handler App', () => {
   let page: AppPage;
 
   beforeEach(() => {
@@ -10,7 +11,7 @@ describe('bannerhandler App', () => {
     page.setStorage('testEnv', '');
   });
 
-  it('should display login page', () => {
+  it('should display login page if unauthenticated', () => {
 
     const pageTitle = page.select('mat-toolbar span').getText();
     const authComponent = page.select('app-login');
@@ -26,7 +27,7 @@ describe('bannerhandler App', () => {
   });
 });
 
-describe('bannerhandler', () => {
+describe('Banner Handler App', () => {
   let page: AppPage;
 
   beforeEach(() => {
@@ -35,12 +36,50 @@ describe('bannerhandler', () => {
     page.setStorage('testEnv', 'true');
   });
 
-  it('should evade login page with test env', () => {
+  it('should evade login page with if test env is specified and display banners', () => {
 
   	const authComponent = page.select('app-login');
   	const bannerComponent = page.select('app-banner');
 
   	expect(authComponent.isPresent()).toBeFalsy();
   	expect(bannerComponent.isPresent()).toBeTruthy();
+  });
+
+  it("should display form when clicking on add banner button", () => {
+
+    const formClass = '.banner-form-card';
+    const btnClass = '.add-banner-floating-btn';
+
+    let form = page.select(formClass);
+    expect(form.getCssValue('visibility')).toBe('hidden');
+
+    page.select(btnClass).click();
+
+    form = page.select(formClass);
+    expect(form.getCssValue('visibility')).toBe('visible');
+  });
+
+  it("should pre-upload a picture and display it in preview in form", () => {
+
+    const previewClass = '.img-container';
+
+    // First we show the banner form
+    const btnShowFormClass = '.add-banner-floating-btn';
+    page.select(btnShowFormClass).click();
+
+    // We expect no preview
+    expect(page.select(previewClass).getCssValue('background-image')).toBe('none');
+
+    // Then we emulate picture upload
+    const fileToUpload = '../src/assets/test/test.jpg';
+    const absolutePath = path.resolve(__dirname, fileToUpload);
+
+    page.select('.upload-btn').sendKeys(absolutePath);
+    page.select('.upload-btn').click();
+
+    // Then we check the preview
+    setTimeout(() => {
+      expect(page.select(previewClass).getCssValue('background-image').toBe('none');
+    }, 5000);
   });
 });
