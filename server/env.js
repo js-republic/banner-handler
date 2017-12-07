fc0b56ae4d3ac959607dee45aa38ae0aa20a43ab
guillaumebarranco <guillaume.barranco8@hotmail.fr>
2017-12-07 16:01:31 +0100

    add s3 bucket in env.js

--- server/env.js
+++ server/env.js
@@ -7,5 +7,6 @@ module.exports = envalid.cleanEnv(process.env, {
   GOOGLE_ALLOWED_GROUP: envalid.str(),
   AWS_ACCESS_KEY_ID: envalid.str(),
   AWS_SECRET_ACCESS_KEY: envalid.str(),
+  AWS_S3_BUCKET: envalid.str(),
   PORT: envalid.port()
 });
