1ef30fcc38491564aa9da69f0e0e8607ec4235da
Mathieu Breton <mathieu.breton@js-republic.com>
2018-02-05 11:10:58 +0100

    add ut on alicia key

--- src/app/banners/aliciakeys.pipe.ts
+++ src/app/banners/aliciakeys.pipe.ts
@@ -1,15 +1,15 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import {Pipe, PipeTransform} from '@angular/core';
 
 @Pipe({
   name: 'aliciaKeys'
 })
 export class AliciaKeys implements PipeTransform {
-  transform(value: any, arg: string[]): string {
-    return value
-      ? Object.entries(value)
-          .filter(([key, value]) => value)
-          .map(([key, value]) => key)
-          .join(', ')
+  transform(obj: any, arg: string[]): string {
+    return obj
+      ? Object.entries(obj)
+        .filter(([key, value]) => !!value)
+        .map(([key]) => key)
+        .join(', ')
       : '';
   }
 }
